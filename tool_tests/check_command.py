50c0f00fe6c8b85b2e15a9064bc56037ac12f97b
maeva <maeva.arroniz@epitech.eu>
2025-04-23 16:15:42 +0200

    [ADD]: change gobuster function to use the rockyou.txt

--- tool_tests/check_command.py
+++ tool_tests/check_command.py
@@ -1,34 +1,36 @@
 import subprocess
 from urllib.parse import urlparse
-
+import subprocess
 
 def run_nmap(url):
     parsed = urlparse(url)
     host = parsed.hostname
-    print("Running nmap on host:", host)
+    print(f"Running Nmap on: {url}")
     try:
-        result = subprocess.run(['nmap', '-sC', '-sV', '-T5', '-Pn', host], capture_output=True, text=True, check=True)
+        result = subprocess.run(['nmap', '-sC', '-sV', '-T5', '-Pn',
+        host], capture_output=True, text=True, check=True)
         return result.stdout
     except subprocess.CalledProcessError as e:
         print(f"Nmap command failed with error code {e.returncode}")
         print(f"Error message: {e.stderr}")
     except FileNotFoundError:
-        print("Error: nmap command not found. Please ensure nmap is installed and in your system's PATH.")
+        print("Error: nmap command not found.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
 
 def run_gobuster(url):
     parsed = urlparse(url)
-    host = parsed.hostname
-    print("Running nmap on host:", host)
+    if not parsed.scheme:
+        url = 'http://' + url
+    print(f"Running Gobuster on: {url}")
     try:
-        result = subprocess.run(['gobuster', 'dir', '-u', host, '-w', host], capture_output=True, text=True, check=True)
+        result = subprocess.run(['gobuster', 'dir', '-u', url, '-w',
+        'wordlists/rockyou_2k.txt', '--exclude-length', '2245'], capture_output=True, text=True, check=True)
+        print("result: ", result.stdout)
         return result.stdout
     except subprocess.CalledProcessError as e:
-        print(f"Gobuster command failed with error code {e.returncode}")
-        print(f"Error message: {e.stderr}")
+        print(f"Gobuster failed: {e.stderr}")
     except FileNotFoundError:
-        print("Error: gobuster command not found. Please ensure nmap is installed and in your system's PATH.")
+        print("Gobuster not found.")
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(f"Unexpected error: {e}")
